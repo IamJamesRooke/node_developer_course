51f597577b32bd418820ba50fd95c83754c85237
IamJamesRooke <me@jamesrooke.blog>
2025-04-16 14:01:54 -0500

    finished 04 webserver chapter

--- 04_webserver/index.js
+++ 04_webserver/index.js
@@ -20,7 +20,14 @@ const friends = [
 
 server.on('request', (req, res) => {
     const items = req.url.split('/');
-    if (items[1] === 'friends') {
+    if (req.method === 'POST' && items[1] === 'friends') {
+        req.on('data', (data) => {
+            const friend = data.toString();
+            console.log('Request:', friend);
+            friends.push(JSON.parse(friend));
+            req.pipe(res);
+        })
+    } else if (req.method === 'GET' && items[1] === 'friends') {
         res.statusCode = 200
         res.setHeader('Content-Type', 'application/json')
         if (items.length === 3) {
@@ -29,7 +36,7 @@ server.on('request', (req, res) => {
         } else {
             res.end(JSON.stringify(friends));
         }
-    } else if (items[1] === 'messages') {
+    } else if (req.method === 'GET' && items[1] === 'messages') {
         res.setHeader('Content-Type', 'text/html')
         res.write('<html>');
         res.write('<body>');
