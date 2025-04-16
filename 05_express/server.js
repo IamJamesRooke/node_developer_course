667dd93e16491ce7a141b4bad4a4be01a7a3a67f
IamJamesRooke <me@jamesrooke.blog>
2025-04-16 16:59:49 -0500

    app.get('/friends/:friendId'

--- 05_express/server.js
+++ 05_express/server.js
@@ -1,12 +1,41 @@
 const express = require('express');
-
 const app = express();
-
 const PORT = 3000;
+const friends = [
+    {
+        id: 0,
+        name: "Friend 0"
+    },
+    {
+        id: 1,
+        name: "Friend 1"
+    },
+    {
+        id: 2,
+        name: "Friend 2"
+    },
+    {
+        id: 3,
+        name: "Friend 3"
+    },
+];
 
 app.get('/', (req, res) => {
-    res.send('Testing');
+    res.json(friends);
 });
+
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    if (friend) {
+        res.status(200).json(friend);
+    } else {
+        res.status(404).json({
+            error: 'Friend does not exist'
+        });
+    }
+});
+
 
 app.get('/messages', (req, res) => {
     res.send('Messages');
