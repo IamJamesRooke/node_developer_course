ed2a6f4f1e8d67584e5ee1d99d25b67d171ee476
IamJamesRooke <me@jamesrooke.blog>
2025-04-17 15:02:10 -0500

    basic site with HandleBars view engine

--- 05_express/server.js
+++ 05_express/server.js
@@ -6,6 +6,10 @@ const PORT = 3000;
 // Utilities
 const path = require('path');
 
+// Setting up Handle Bars (hbs) as view engine.
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
 // Router Imports
 const friendsRouter = require('./routes/friends.router')
 const messagesRouter = require('./routes/messages.router')
@@ -18,20 +22,27 @@ app.use((req, res, next) => {
     console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 })
 
+// Serving basic website
 app.use('/site', express.static(path.join(__dirname, 'public')))
 
 // JSON Middleware
 app.use(express.json());
 
+// Home Page
+app.get('/', (req, res) => {
+    res.render('index.hbs', {
+        title: "Lake",
+        caption: "This is a lake."
+    });
+});
+
 // Router Middleware
 app.use('/friends', friendsRouter);
 app.use('/messages', messagesRouter);
 
-// Home Page
-app.get('/', (req, res) => {
-    res.send('HOME PAGE');
-});
 
+
+// Listening on PORT
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`)
 })
