e9fe685d70a737d5b003af96ae8ead35bded1cc8
IamJamesRooke <me@jamesrooke.blog>
2025-04-19 12:19:21 -0500

    logging request with morgan added

--- 06_nasa_project/server/src/app.js
+++ 06_nasa_project/server/src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets.router')
 
@@ -10,7 +11,9 @@ const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000'
-}))
+}));
+
+app.use(morgan('combined'))
 
 app.use(express.json()); // Parses incoming JSON requests
 app.use(express.static(path.join(__dirname, '..', 'public')))
@@ -18,6 +21,6 @@ app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use('/planets', planetsRouter); // Mounts the planets router at the '/planets' path
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
-})
+});
 
 module.exports = app;
