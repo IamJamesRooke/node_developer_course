3bfd735861520b784639b831a4796a56c0e90274
IamJamesRooke <me@jamesrooke.blog>
2025-04-28 15:38:52 -0500

    basic mongoose/mongo connection with .env

--- nasa_project/server/src/server.js
+++ nasa_project/server/src/server.js
@@ -1,16 +1,28 @@
+require('dotenv').config();
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000;
+
+const MONGO_URL = process.env.MONGO_URL;
+
 const server = http.createServer(app);
 
-async function startServer(){
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error(err)
+})
+
+async function startServer() {
+    await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
     
-    server.listen(PORT, () => {
-        console.log('Listening on PORT', PORT)
-    });
+    server.listen(PORT, () => {console.log('Listening on PORT', PORT)});
 }
 
 startServer();
