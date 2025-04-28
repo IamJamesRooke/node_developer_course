e918884d139e259fe5ee37d37d1cfe8c7a45cdce
IamJamesRooke <me@jamesrooke.blog>
2025-04-28 07:33:57 -0500

    learning pm2

--- 06_performance-example/server.js
+++ 06_performance-example/server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const cluster = require('cluster');
-const os = require('os');
 
 const app = express();
 
@@ -22,15 +20,5 @@ app.get('/timer', (req, res) => {
 });
 
 console.log('Running server.js...')
-if (cluster.isMaster) {
-    console.log('Master Started')
-    const NUM_WORKERS = os.cpus().length;
-    for (let i = 0; i < NUM_WORKERS; i++) {
-        cluster.fork();
-    }
-
-} else {
-    console.log('Worker Process Started');
-    app.listen(3000, () => {console.log('http://localhost:3000')});
-
-}
+console.log('Worker Process Started');
+app.listen(3000, () => {console.log('http://localhost:3000')});
