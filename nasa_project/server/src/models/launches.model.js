c74d4dd18cbc530d524cefb072d58cdce18f610e
IamJamesRooke <me@jamesrooke.blog>
2025-05-05 10:40:58 -0500

    new launch connected to mongo

--- nasa_project/server/src/models/launches.model.js
+++ nasa_project/server/src/models/launches.model.js
@@ -1,9 +1,8 @@
 const launchesDatabase = require('./launches.mongo');
 const planets = require('./planets.mongo');
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launches = new Map();
-
-let latestFlightNumber = 100;
 
 const launch = {
     flightNumber: 100,
@@ -23,11 +22,21 @@ function existsLaunchWithId(launchId) {
     return launches.has(launchId);
 }
 
-
-
 async function getAllLaunches() {
     return await launchesDatabase
         .find({}, {'__id': 0, '__v': 0});
+}
+
+async function getLatestFlightNumber() {
+    const latestLaunch = await launchesDatabase
+        .findOne({})
+        .sort('-flightNumber');
+
+    if (!latestLaunch) {
+        return DEFAULT_FLIGHT_NUMBER;
+    }
+
+    return latestLaunch.flightNumber;
 }
 
 async function saveLaunch(launch) {
@@ -40,24 +49,25 @@ async function saveLaunch(launch) {
         throw new Error('No matching planet found.')
     } 
 
-    await launchesDatabase.updateOne({
+    await launchesDatabase.findOneAndUpdate({
         flightNumber: launch.flightNumber,
     }, launch, {
         upsert: true,
     });
 }
 
-function addNewLaunch(launch) {
-    latestFlightNumber++;
-    launches.set(
-        latestFlightNumber,
-        Object.assign(launch, {
-            success: true,
-            upcoming: true,
-            customer: ['ZTM', 'NASA'],
-            flightNumber: latestFlightNumber,
-        })
-    );
+async function scheduleNewLaunch(launch) {
+
+    const newFlightNumber = await getLatestFlightNumber() + 1;
+
+    const newLaunch = Object.assign(launch, {
+        success: true,
+        upcoming: true,
+        customers: ['ZTM', 'NASA'],
+        flightNumber: newFlightNumber,
+    });
+
+    await saveLaunch(newLaunch);
 }
 
 function abortLaunchById(launchId) {
@@ -70,6 +80,6 @@ function abortLaunchById(launchId) {
 module.exports = {
     existsLaunchWithId,
     getAllLaunches,
-    addNewLaunch,
+    scheduleNewLaunch,
     abortLaunchById,
 }
--- nasa_project/server/src/routes/launches/launches.controller.js
+++ nasa_project/server/src/routes/launches/launches.controller.js
@@ -1,6 +1,6 @@
 const { 
     getAllLaunches,
-    addNewLaunch,
+    scheduleNewLaunch,
     existsLaunchWithId,
     abortLaunchById
  } = require('../../models/launches.model')
@@ -10,7 +10,7 @@ async function httpGetAllLaunches(req, res) {
 
 }
 
-function httpAddNewLaunch(req, res) {
+async function httpAddNewLaunch(req, res) {
     const launch = req.body;
     
     if(!launch.mission || !launch.launchDate || !launch.rocket || !launch.target) {
@@ -26,7 +26,7 @@ function httpAddNewLaunch(req, res) {
         });
     }
 
-    addNewLaunch(launch);
+    await scheduleNewLaunch(launch);
     return res.status(201).json(launch);
 }
 
