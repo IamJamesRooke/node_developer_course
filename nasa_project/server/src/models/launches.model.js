63b0dd9ea15fda80e046416ebb9a370459b0a316
IamJamesRooke <me@jamesrooke.blog>
2025-05-05 11:24:30 -0500

    abort launch functionality with mongo

--- nasa_project/server/src/models/launches.model.js
+++ nasa_project/server/src/models/launches.model.js
@@ -2,8 +2,6 @@ const launchesDatabase = require('./launches.mongo');
 const planets = require('./planets.mongo');
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launches = new Map();
-
 const launch = {
     flightNumber: 100,
     mission: "Kepler Exploration X",
@@ -18,8 +16,10 @@ const launch = {
 saveLaunch(launch);
 // launches.set(launch.flightNumber, launch);
 
-function existsLaunchWithId(launchId) {
-    return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+    return await launchesDatabase.findOne({
+        flightNumber: launchId,
+    });
 }
 
 async function getAllLaunches() {
@@ -70,11 +70,16 @@ async function scheduleNewLaunch(launch) {
     await saveLaunch(newLaunch);
 }
 
-function abortLaunchById(launchId) {
-    const aborted = launches.get(launchId)
-    aborted.upcoming = false;
-    aborted.success = false;
-    return aborted
+async function abortLaunchById(launchId) {
+    const aborted = await launchesDatabase.updateOne({
+        flightNumber: launchId,
+    }, {
+        upcoming: false,
+        success: false
+    })
+
+    return aborted.modifiedCount === 1;
+
 }
 
 module.exports = {
