005c31c8eaeb84c850d9a703ce2c79bf30079477
IamJamesRooke <me@jamesrooke.blog>
2025-05-08 07:13:45 -0500

    small dotenv change

--- nasa_project/server/src/services/mongo.js
+++ nasa_project/server/src/services/mongo.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-
 require('dotenv').config();
-const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready')
@@ -12,7 +10,7 @@ mongoose.connection.on('error', (err) => {
 })
 
 async function mongoConnect() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
 }
 
 module.exports = {
