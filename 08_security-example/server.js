e80a48288d5ebc58ebdd54fe0edf646cced8c0f5
IamJamesRooke <me@jamesrooke.blog>
2025-05-29 11:43:02 -0500

    signout functionality

--- 08_security-example/server.js
+++ 08_security-example/server.js
@@ -13,33 +13,36 @@ require('dotenv').config();
 const PORT = 3000;
 
 const config = {
-    CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET,
-    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
-    COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
-    callbackURL: '/auth/google/callback',
-    clientID: config.CLIENT_ID,
-    clientSecret: config.CLIENT_SECRET,
-}
+  callbackURL: '/auth/google/callback',
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
-    console.log('Google profile', profile)
-    done(null, profile);
+  console.log('Google profile', profile);
+  done(null, profile);
 }
 
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
-// Save session to cookie.
+// Save the session to the cookie
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
-// Read session from cookie.
+// Read the session from the cookie
 passport.deserializeUser((id, done) => {
-    done(null, id);
+  // User.findById(id).then(user => {
+  //   done(null, user);
+  // });
+  done(null, id);
 });
 
 const app = express();
@@ -47,74 +50,81 @@ const app = express();
 app.use(helmet());
 
 app.use(cookieSession({
-    name: 'session',
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: [ config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+  name: 'session',
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
 }));
-
-// Don't remove this code.
 app.use((req, res, next) => {
-    if (req.session && !req.session.regenerate) {
-        req.session.regenerate = (cb) => {
-            cb();
-        }
-    }
-    if (req.session && !req.session.save) {
-        req.session.save = (cb) => {
-            cb();
-        }
-    }
-    next();
+  // Stub out missing regenerate and save functions.
+  // These don't make sense for client side sessions.
+  if (req.session && !req.session.regenerate) {
+    req.session.regenerate = (cb) => {
+      cb();
+    };
+  }
+  if (req.session && !req.session.save) {
+    req.session.save = (cb) => {
+      cb();
+    };
+  }
+  next();
 });
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 function checkLoggedIn(req, res, next) {
-    const isLoggedIn = true; //TODO
-    if (!isLoggedIn) {
-        return res.status(401).json({
-            error: 'You must log in!',
-        });
-    }
-    next();
+  console.log('Current user is:', req.user);
+  const isLoggedIn = req.isAuthenticated() && req.user;
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: 'You must log in!',
+    });
+  }
+  next();
 }
 
-app.get('/auth/google', 
-    passport.authenticate('google', {
-        scope: ['email']
-    })
-);
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['email'],
+  }));
 
-app.get('/auth/google/callback', 
-    passport.authenticate('google', {
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
     failureRedirect: '/failure',
-    success: '/',
+    successRedirect: '/',
     session: true,
-    }), 
-    (req, res) => {
-        console.log('Google Called Back')
-    }
+  }),
+  (req, res) => {
+    console.log('Google called us back!');
+  }
 );
 
-app.get('/auth/logout', (req, res) => {})
+app.get('/auth/logout', (req, res, next) => {
+  //Removes req.user and clears any logged in session
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
 
 app.get('/secret', checkLoggedIn, (req, res) => {
-    return res.send('Password: password');
-})
+  return res.send('Your personal secret value is 42!');
+});
 
 app.get('/failure', (req, res) => {
-    return res.send('Failed to login.')
-})
+  return res.send('Failed to log in!');
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 https.createServer({
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem'),
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
 }, app).listen(PORT, () => {
-    console.log(`Listening on https://localhost:${PORT}...`)
-    console.log(`Secret found on https://localhost:${PORT}/secret`)
-})
+  console.log(`Listening on port ${PORT}...`);
+});
